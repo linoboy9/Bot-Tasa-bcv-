02089b9a718d95d4d7ca2851a51438be2191f6c3
linoboy9 <142373926+linoboy9@users.noreply.github.com>
2026-08-13 18:30:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+import threading
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot BCV Activo"
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
+threading.Thread(target=run_web, daemon=True).start()
+
 import os
 import requests
 from bs4 import BeautifulSoup
